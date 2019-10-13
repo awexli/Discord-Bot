a0d73893280a40195c64d649aa435920708c53ff
Alex Li <li_alex1415@hotmail.com>
2019-10-12 18:12:00 -0700

    Added: Role Info command and Display Role command

--- commands/assignRole.js
+++ commands/assignRole.js
@@ -1,17 +1,25 @@
 module.exports = {
-    name: 'role',
+    name: 'role+',
     description: "Assigns a guild member to role",
     execute(message, args) {
 
-        // check for permission later
+        if (!message.member.hasPermission("MANAGE_ROLES")) {
+            return message.reply("You don't have the permission to do that.");
+        }
+
         if (!args.length || args.length > 2) {
-            return message.reply(`\`p-role\` \`<@username>\` \`<role>\``);
+            return message.reply(`\`p-role+\` \`<@username>\` \`<role>\``);
         }
 
         let gMember = message.mentions.members.first();
         if (!gMember) {
             return message.reply("Couldn't find that user.");
         }
+        /* // displays all user roles
+        let embed = new Discord.RichEmbed()
+            // ... all the other stuff ...
+            .addField('Roles:', gMember.roles.map(r => `${r}`).join(' | '), true)
+        */
 
         let roleName = args[1];
         var memberRole = message.guild.roles.find(role => role.name == roleName);
@@ -20,9 +28,7 @@ module.exports = {
         }
 
         gMember.addRole(memberRole.id);
-
         message.reply(`\`${gMember.displayName}\` has been added to the role, \`${roleName}\``);
-
-
+        
     },
 };
---
+++ commands/roleInfo.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'role',
+    description: 'Displays info on how to use role commands',
+    execute(message) {
+        const embed = new Discord.RichEmbed()
+            .setTitle('Role Commands')
+            .setColor(0xFF0000)
+            .addField('p-role+ <@username> <role name>', 'Assign user a role.')
+            .addField('p-role- <@username> <role name>', 'Remove role from user.')
+            .addField('p-role? <@username>', 'Display user role(s).');
+        message.channel.send(embed);
+
+    },
+};
